11a3dd936e0de4ac932736ea8a2069935bb16b6b
huhubun <nongzc@outlook.com>
2017-08-28 00:32:40 +0800

    #1 Add WOL sample

---
+++ samples/Net/Bun.Toolkit.Net.Samples.WakeOnLAN/Program.cs
@@ -0,0 +1,16 @@
+﻿using Bun.Toolkit.Net.Extensions;
+using System.Net.NetworkInformation;
+
+namespace Bun.Toolkit.Net.Samples.WakeOnLAN
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Magic Packet Sample
+
+            var macAddress = PhysicalAddress.Parse("11-22-33-44-55-66");
+            macAddress.Wake();
+        }
+    }
+}
---
+++ samples/Net/Bun.Toolkit.Net.Samples.WakeOnLAN/README.md
@@ -0,0 +1,13 @@
+﻿# BunToolkit Walk-On-LAN Sample
+
+## Magic Packet
+```CSharp
+using Bun.Toolkit.Net.Extensions;
+using System.Net.NetworkInformation;
+```
+
+
+```CSharp
+var macAddress = PhysicalAddress.Parse("11-22-33-44-55-66");
+macAddress.Wake();
+```
